775cc5a0a1ebfbfddfbbf0e454e2beac27f46db1
Juho Vepsalainen <bebraw@gmail.com>
2018-10-04 12:43:22 +0200

    chore: Add code splitting demo

--- demo/src/component.js
+++ demo/src/component.js
@@ -1,7 +1,16 @@
 export default (text = "Hello world") => {
   const element = document.createElement("div");
 
-  element.innerHTML = text;
+  element.textContent = text;
+
+  element.onclick = () =>
+    import("./lazy")
+      .then(lazy => {
+        element.textContent = lazy.default;
+      })
+      .catch(err => {
+        console.error(err);
+      });
 
   return element;
 };
