b8fab9a6e4bc13711d611042762f46ef4c0b1ed3
Juho Vepsalainen <bebraw@gmail.com>
2018-10-04 11:03:53 +0200

    chore: Port to webpack-merge

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,9 +1,32 @@
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack demo",
-    }),
-  ],
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
+  },
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
